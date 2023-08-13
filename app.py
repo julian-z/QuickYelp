2673c320c331cd2dd6e4610cf7b0c7c4fbd277b1
Julian Zulfikar <julianzulfikar@gmail.com>
2023-08-12 20:09:30 -0700

    Added pop-up input verification, first front-end draft

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template, request, jsonify
 import random  # For random replies
 
+import requests
+from collections import defaultdict
+from unidecode import unidecode
+from lxml import html
+from html import unescape
+import json
+import time
+import re
+from random import randint
+
+import os
+from api_key import API_KEY
+from langchain.document_loaders import TextLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.chat_models import ChatOpenAI
+
+from shell import validate_url
+
+
+os.environ["OPENAI_API_KEY"] = API_KEY
 app = Flask(__name__)
 
 
@@ -12,32 +32,49 @@ def interact_with_openai(query):
     return random.choice(replies)
 
 
+ai_replies = [
+    "Oops! It seems like the Yelp URL you entered is not valid. Please try again.",
+    "Sorry, but the Yelp URL you provided is incorrect or unsupported. Please double-check and retry.",
+    "Uh-oh! The Yelp URL you entered doesn't seem to work. Please provide a valid URL and try again.",
+    "We encountered an issue with the Yelp URL you provided. Please make sure it's correct and retry.",
+    "Hmmm... It appears the Yelp URL you entered is not valid. Kindly check it and try once more."
+]
 @app.route("/", methods=["GET", "POST"])
 def index():
-    chat_history = []  # Initialize an empty chat history
+    chat_history = []
     
     if request.method == "POST":
         if "url" in request.form and "num_pages" in request.form:
-            # This is the initial form submission for starting the chatbot
+
+            # Initial form submission for starting the chatbot
             url = request.form.get("url")
             num_pages = int(request.form.get("num_pages"))
             
+            # Re-prompt user if url or num_pages is not valid
+            if not validate_url(url):
+                return render_template("index.html", error_message=ai_replies[random.randint(0, 4)])
+
             # Perform data scraping here using the provided URL and num_pages
-            pass
-            
-            # For demonstration purposes, we'll use mock data
-            business_data = {
-                "name": "Example Business",
-                "history": "A brief history of the business...",
-                "specialties": "Specialties of the business...",
-                "reviews": {
-                    "5": ["Excellent service!", "Highly recommended!"],
-                    "4": ["Good experience.", "Will visit again."],
-                    "3": ["Average service.", "Could be better."],
+            else:
+                import time
+                time.sleep(5)
+                
+                pass
+                
+                # For demonstration purposes, we'll use mock data
+                business_data = {
+                    "name": "Example Business",
+                    "history": "A brief history of the business...",
+                    "specialties": "Specialties of the business...",
+                    "reviews": {
+                        "5": ["Excellent service!", "Highly recommended!"],
+                        "4": ["Good experience.", "Will visit again."],
+                        "3": ["Average service.", "Could be better."],
+                    }
                 }
-            }
-            
-            return render_template("chat.html", business_data=business_data, chat_history=[], chatbot_reply=None)
+                
+                return render_template("chat.html", business_data=business_data, chat_history=[], chatbot_reply=None)
+        
         else:
             # This is a question asked in the chat box
             query = request.form.get("query")
@@ -55,7 +92,7 @@ def index():
             
             return jsonify({"chat_history": chat_history, "chatbot_reply": chatbot_reply})
     
-    return render_template("index.html")
+    return render_template("index.html", error_message="")
 
 
 if __name__ == "__main__":
